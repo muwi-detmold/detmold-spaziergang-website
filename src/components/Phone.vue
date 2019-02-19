2b8639111b019165247c97024a382e5d793cedb9
Daniel Röwenstrunk <roewenstrunk@edirom.de>
2019-02-19 15:32:18 +0100

    first draft

---
+++ src/components/Phone.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="phone-div">
+    <img src="../assets/phone.png" alt="Smart Phone" id="phone"/>
+    <div id="ds-app">
+    <iframe src="http://localhost:8082/">
+
+    </iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Phone'
+}
+</script>
+
+<style scoped lang="scss">
+  #phone-div {
+    position: relative;
+  }
+
+  #ds-app {
+    width: 377px;
+    height: 668px;
+    position: absolute;
+    top: 200px;
+    left: calc(50% - 188px);
+  }
+
+  #ds-app iframe {
+    border: none;
+    width: 100%;
+    height: 100%;
+    overflow-y: hidden;
+  }
+
+  #phone {
+    position: relative;
+    top: 0;
+    width: 700px;
+    margin: 0 auto;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Phone></Phone>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Phone from '@/components/Phone.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Phone
   }
 }
 </script>
