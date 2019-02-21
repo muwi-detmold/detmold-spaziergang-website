79c84367d963e08a146ac351f177e7cf33d62990
Daniel Röwenstrunk <roewenstrunk@edirom.de>
2019-02-21 10:45:55 +0100

    Path changed

--- src/components/Phone.vue
+++ src/components/Phone.vue
@@ -2,7 +2,7 @@
   <div class="phone-div">
     <img src="../assets/phone.png" alt="Smart Phone" id="phone"/>
     <div id="ds-app">
-    <iframe src="https://detmold-spaziergang.uni-paderborn.de:8080">
+    <iframe src="https://detmold-spaziergang.uni-paderborn.de/phone">
 
     </iframe>
     </div>
